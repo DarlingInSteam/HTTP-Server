fa881541bb66f4e499dd9ce3c2ae4faae898192e
Darling <rombk2711@gmail.com>
2024-11-08 22:09:45 +0700

    [+]: controllers, service - Change code style (lab db)

--- src/main/java/com/example/httpserver/controller/AuthController.java
+++ src/main/java/com/example/httpserver/controller/AuthController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    @Autowired
-    private AuthService authService;
+    @Autowired private AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity registerUser(@RequestParam String username, @RequestParam String password) {
